836e8cefdab8aad03b04d2702f67f317487e5975
styled <smyvensestime@gmail.com>
2023-04-14 17:21:34 -0400

    fixed percentages

--- init-build-webcam-ai/src/components/BarGraphLine.jsx
+++ init-build-webcam-ai/src/components/BarGraphLine.jsx
@@ -6,11 +6,13 @@ export default ({ emoji, percentage }) => {
             <h3 className="text-3xl text-left w-10">{emojiIcon[emoji]}</h3>
             <span className="bg-blue-400 h-4 w-full rounded-md overflow-hidden">
                 <div
-                    style={{ width: `${percentage}%` }}
+                    style={{ width: `${percentage * 100}%` }}
                     className="bg-blue-700 h-4 w-full rounded-l-md transition-[width] ease-in-out duration-300"
                 />
             </span>
-            <h3 className="w-10 text-right">{percentage}%</h3>
+            <h3 className="w-10 text-right">
+                {(percentage * 100).toFixed(2)}%
+            </h3>
         </div>
     );
 };
